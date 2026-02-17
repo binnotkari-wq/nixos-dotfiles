d2c22b164e2e51b276a8a5ef79199986c166ea3c
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-02-17 21:54:35 +0100

    Auto-sync [dell-5485] : 2026-02-17 21:54:35

--- software-setup/programs/CLI_tools.nix
+++ software-setup/programs/CLI_tools.nix
@@ -5,5 +5,6 @@
     git         # interface de versionning
     wget        # téléchargement de fichiers par http
     pciutils    # pour la commande lspci
+    compsize    # analyse système de fichier btrfs : sudo compsize /nix
   ];
 }
