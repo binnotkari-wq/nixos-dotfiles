2ab0140af867c9543aa97369912ca63a147f7c7a
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-02-26 20:41:43 +0100

    Update gaming.nix

--- software-setup/programs/gaming.nix
+++ software-setup/programs/gaming.nix
@@ -4,6 +4,15 @@
   # --- LACT pour la gestion GPU AMD / Nvidia / intel ---
   services.lact.enable = true;
 
+  programs.gamemode.enable = true;
 
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = false; # on utilise la session custom à la place
+    extraPackages = with pkgs; [ mangohud ];
+  };
 
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
 }
