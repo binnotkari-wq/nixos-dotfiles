e995ed724bf94e6073d1c1f3df82d49a95d06578
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-02-26 21:11:39 +0100

    Update len-x240.nix

--- len-x240.nix
+++ len-x240.nix
@@ -1,18 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  # --- VERSION NIXOS A INSTALLER ---
-  system.stateVersion = "25.11";
-  
-  
-  # NE PAS MODIFIER CES DECLARATIONS
-  
-  # --- IDENTIFICATION SYSTEME ---
-  networking.hostName = "len-x240";
-  
-  # --- DEPLOIEMENTS ---
+  # --- MODULES ---
   imports = [
-    ./hardware-support/len-x240_hardware-support.nix # paramètres matériel - spécifique machine
-    ./software-setup/common-base.nix # paramètres OS, bureau, applications et compte utilisateur - pour toute machine
+
   ];
+
+  # --- TUNING ---
+  # Le X240 est parfaitement stable en stress-test avec ces valeurs (et le bosst est maintenu, avec une température de moins de 70 degrés!)
+  services.undervolt = {
+    enable = true;
+    coreOffset = -40;       # Valeur en mV (-80 pour commencer : kernel panic lors du débranchement de l'alim)
+    gpuOffset = -40;        # L'iGPU peut aussi être undervolté
+    uncoreOffset = -40;     # Contrôleur mémoire, etc.
+    analogioOffset = 0;     # Généralement laissé à 0
+
+    # Paramètre optionnel : définit la limite de température avant throttling
+    # temp = 75;
+  };
 }
