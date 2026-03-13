d441534b095ae0bb011c7117816ec9733e9656dd
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-03-13 23:49:33 +0100

    Auto-sync [nixos] : 2026-03-13 23:49:33

--- modules/OS-optimizations_filesystems.nix
+++ modules/OS-optimizations_filesystems.nix
@@ -2,8 +2,9 @@
 
 {
   # --- Optimisation des volumes BTRFS (si existants) sauf l'éventuel sous-volume /swap (d'après options de montages décrites dans https://wiki.nixos.org/wiki/Btrfs )
+  # Ce fichier est inutile avec une installation scriptée (options BTRFS appliquées à l'installation)
   # mkAfter garantit que ces options sont ajoutées APRES celles du hardware-configuration
-  # Il faudra y ajouter les éventuels autres volumes btrfs en cas de partitionnement custom, comme ils apparaissent dans hardware-configuration.nix
+  # Il faudra y ajouter les éventuels autres volumes btrfs en cas de partitionnement custom dans Calamares, comme ils apparaissent dans hardware-configuration.nix
 
   fileSystems."/" = {
     options = lib.mkAfter [ "noatime" "compress=zstd" "ssd" "discard=async" ];
@@ -11,14 +12,5 @@
 
   fileSystems."/home" = {
     options = lib.mkAfter [ "noatime" "compress=zstd" "ssd" "discard=async" ];
-  };
-
-
-  # --- Optimisations des volumes LUKS
-  # Il faut récupérer la valeur "luks-xxxxx..." dans hardware-configuration.nix
-
-  boot.initrd.luks.devices."luks-400c5604-0663-4e0b-ab4e-8475af6212b8" = {
-    allowDiscards = true;
-    bypassWorkqueues = true;
   };
 }
--- vm.nix
+++ vm.nix
@@ -5,9 +5,7 @@
   imports = [
   ./modules/tests.nix
   ./modules/configuration_addons.nix
-  ./modules/filesystems-settings.nix
-  # ./modules/stateless-light.nix # commenter si on utilise impermanence
-  ./modules/OS-functions_impermanence.nix # NECESSITE INSTALLATION PAR SCRIPT. Commenter si on utilise stateless, sur installation classique.
+  # ./modules/filesystems-settings.nix # laisser commenté si l'installation a été faite depuis le script
   ./modules/user_apps.nix
   ];
 
