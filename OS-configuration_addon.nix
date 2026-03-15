e222dc7ab69636d21a01a177a18029cdcf8a6204
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-03-15 23:13:44 +0100

    Auto-sync [nixos] : 2026-03-15 23:13:44

--- OS-configuration_addon.nix
+++ OS-configuration_addon.nix
@@ -14,6 +14,16 @@
     bypassWorkqueues = true;
   };
 
+  # --- SWAP / ZRAM --- 
+  # Avec 8 Go de RAM, un ZRAM (swap en ram compressée) bien dimensionné suffit largement pour une machine de bureau ou un laptop classique.
+  zramSwap.enable = true;
+  zramSwap.priority = 100; # sera utilisé en priorité avant le swap sur disque (s'il existe)
+  zramSwap.memoryPercent = 50; # Utilise jusqu'à 50% de la RAM
+  boot.kernel.sysctl = {
+  "vm.swappiness" = 100; # Le noyau va commencer à envoyer des plages mémoire en zram sans attendre la saturation
+  };
+  # swapDevices = [ { device = "/swapfile"; size = 2048; } ]; pas utile avec le zram. Et en l'état, pas compatible si sur un volume BTRFS compressé et avec COW.
+
   # --- MATÉRIEL & SERVICES ---
   security.apparmor.enable = true; # les flatpaks ont un profil apparmor intégré : ils vont automatiquement profiter de apparmor. L'impact d'apparmor sur les performances est imperceptible.
   services.flatpak.enable = true;
