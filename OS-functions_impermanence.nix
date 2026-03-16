b45222ba8a562bc39e03dbf2b1c57f8fe9104762
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-03-16 20:10:12 +0100

    Auto-sync [nixos] : 2026-03-16 20:10:12

--- OS-functions_impermanence.nix
+++ OS-functions_impermanence.nix
@@ -1,77 +1,39 @@
 { config, lib, pkgs, ... }:
 
 {
+
+  imports = [
+    (builtins.fetchTarball { url = "https://github.com/nix-community/impermanence/archive/master.tar.gz";} + "/nixos.nix") # module impermanence à intégrer dans le store
+  ];
+
   # --- SYSTEMES DE FICHIERS : options pour permettre le montage avant que le système ne cherche les fichiers persistés ---
   fileSystems."/nix".neededForBoot = true ;
   fileSystems."/persist".neededForBoot = true ;
 
-  # --- FICHIERS A PERSISTER (via Symlinks)
-  environment.etc = {
-    "machine-id".source = "/persist/etc/machine-id";
-    "adjtime".source = "/persist/etc/adjtime";
-    "shadow".source = "/persist/etc/shadow";
-    "passwd".source = "/persist/etc/passwd";
-    "group".source = "/persist/etc/group";
-    "subuid".source = "/persist/etc/subuid";
-    "subgid".source = "/persist/etc/subgid";
-  };
 
-  # --- DOSSIERS A PERSISTER (via Bind Mounts)
-  fileSystems = {
-
-    # Matériel et GPU
-    "/etc/lact" = { device = "/persist/etc/lact"; fsType = "none"; options = [ "bind" ]; };
-
-    # Réseau
-    "/etc/NetworkManager/system-connections" = {
-      device = "/persist/etc/NetworkManager/system-connections";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    # Configuration Nix (pour ne pas perdre tes .nix !)
-    "/etc/nixos" = { device = "/persist/etc/nixos"; fsType = "none"; options = [ "bind" ]; };
-
-    "/var/lib/bluetooth" = {
-      device = "/persist/var/lib/bluetooth";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    "/var/lib/cups" = {
-      device = "/persist/var/lib/cups";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    "/var/lib/flatpak" = {
-      device = "/persist/var/lib/flatpak";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    "/var/lib/fwupd" = {
-      device = "/persist/var/lib/fwupd";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    "/var/log" = {
-      device = "/persist/var/log";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    "/var/lib/NetworkManager" = {
-      device = "/persist/var/lib/NetworkManager";
-      fsType = "none";
-      options = [ "bind" ];
-    };
-
-    "/var/lib/nixos" = {
-      device = "/persist/var/lib/nixos";
-      fsType = "none";
-      options = [ "bind" ];
-    };
+  # --- ELEMENTS A PERSISTER ---
+  # On utilise le sous-volume /persist pour stocker les rares fichiers de /etc et /var à conserver entre chaque démarrage.
+  # Les bind mount seront créés d'après cette liste.
+  # Si /home n'est pas sur une partion ou des sous-volume btrfs disincts, il faut les lister ici (et /nix serait de toute façon persistant puisque ce serait le point de montage de la partition)
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/etc/lact"
+      "/etc/NetworkManager/system-connections"
+      "/etc/nixos"
+      "/var/lib/bluetooth"
+      "/var/lib/cups"
+      "/var/lib/fwupd"
+      "/var/lib/flatpak"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/var/lib/NetworkManager"
+      "/var/log"
+    ];
+    
+    files = [
+      "/etc/machine-id"
+      "/etc/adjtime"
+    ];
   };
 }
--- machine-vm.nix
+++ machine-vm.nix
@@ -5,7 +5,6 @@
   imports = [
   #./modules/tests.nix
   # ./modules/OS-optimizations_stateless.nix # commenter si on utilise impermanence
-  # ./modules/OS-optimizations_filesystems.nix # commenter si l'installation a été fite depuis le script
   ];
 
   # --- TUNING ---
