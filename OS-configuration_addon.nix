89ee745151bde8fd8df7b101f32ff92935b14a4b
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-03-28 00:42:13 +0100

    Auto-sync [2a02-8429-3054-0701-22bb-160e-0de8-89ce.rev.sfr.net] : 2026-03-28 00:42:13

--- OS-configuration_addon.nix
+++ OS-configuration_addon.nix
@@ -3,51 +3,6 @@
 # Nixos active de base le firewall. Inutile de le déclarer.
 
 {
-  # --- BOOTLOADER & KERNEL ---
-  boot.kernelParams = [ "quiet" "splash" "loglevel=3" "rd.systemd.show_status=false" ];
-  boot.plymouth.enable = true;
-  boot.initrd.systemd.enable = true; # integration de systemd dans l'initrd, Gestion native, systemd-ask-password-plymouth.service prend en charge la demande du mot de passe. Systemd démarre les pilotes graphiques et Plymouth de manière beaucoup plus propre et synchronisée que le script par défaut. C'est la méthode utilisée par Fedora ou Ubuntu.
 
-  # --- OPTIONS LUKS ---
-  boot.initrd.luks.devices."cryptroot" = {
-    allowDiscards = true;
-    bypassWorkqueues = true;
-  };
-
-  # --- SWAP / ZRAM --- 
-  # swapDevices = [ { device = "/swapfile"; size = 2048; } ]; pas utile avec le zram. Et en l'état, pas compatible si sur un volume BTRFS compressé et avec COW.
-  # Avec 8 Go de RAM, un ZRAM (swap en ram compressée) bien dimensionné suffit largement pour une machine de bureau ou un laptop classique.
-  zramSwap.enable = true;
-  zramSwap.priority = 100; # sera utilisé en priorité avant le swap sur disque (s'il existe)
-  zramSwap.memoryPercent = 50; # Utilise jusqu'à 50% de la RAM
-  boot.kernel.sysctl = {
-  "vm.swappiness" = 100; # Le noyau va commencer à envoyer des plages mémoire en zram sans attendre la saturation
-  };
-
-  # --- BTRFS --- 
-  # --- Optimisation des volumes BTRFS d'après options de montages décrites dans https://wiki.nixos.org/wiki/Btrfs
-  # Ces options seront inutiles avec une installation scriptée (options BTRFS appliquées à l'installation)
-  # mkAfter garantit que ces options sont ajoutées APRES celles du hardware-configuration
-  # Il faudra y ajouter les éventuels autres volumes btrfs en cas de partitionnement custom dans Calamares, comme ils apparaissent dans hardware-configuration.nix
-
-  fileSystems."/" = {
-    options = lib.mkAfter [ "noatime" "compress=zstd" "ssd" "discard=async" ];
-  };
-
-  fileSystems."/home" = {
-    options = lib.mkAfter [ "noatime" "compress=zstd" "ssd" "discard=async" ];
-  };
-
-  # --- MATÉRIEL & SERVICES ---
-  security.apparmor.enable = true; # les flatpaks ont un profil apparmor intégré : ils vont automatiquement profiter de apparmor. L'impact d'apparmor sur les performances est imperceptible.
-  services.flatpak.enable = true;
-  hardware.bluetooth.enable = true;
-  hardware.graphics.enable = true; # Vulkan
-
-  # --- UTILISATEUR ---
-  users.users.benoit = {
-    uid = 1000; # pour s'assurer qu'on sera bien bénéficiaire des droits sur /home dans le cas d'une réinstallation où /home est conservé
-    extraGroups = [ "video" "render" "lp" "scanner" ];
-  };
 
 }
